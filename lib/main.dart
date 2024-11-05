6c76e46f766b790e83dd9500991cbb808064c894
Isosa9 <sosayorleni72@gmail.com>
2024-11-04 20:25:56 -0600

    commit main

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,19 @@
+// main.dart
 import 'package:flutter/material.dart';
+import 'router.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp.router(
+      title: 'Aplicación con Drawer',
+      routerConfig: router,
     );
   }
 }
