b2f1d12634c3c5048249a6de96521cb214c8feb0
Isosa9 <sosayorleni72@gmail.com>
2024-11-04 21:34:31 -0600

    commit quinto

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'router.dart';
 
 void main() {
-  runApp(const Newns());
+  runApp(const News());
 }
 
 class News extends StatelessWidget {
@@ -24,12 +24,15 @@ class News extends StatelessWidget {
                   height: 250.0,
                   width: 350.0,
                   child: Image.aseesimagenes(Noticia.png),
+                   fit BoxFit.fitWidth,
                 ),
+
                  SizedBox(
                   height: 250.0,
                   width: 350.0,
                   child: Image.network(
                     'https://www.google.com/url?sa=i&url=https%3A%2F%2Fhumanidades.com%2Fnoticia%2F&psig=AOvVaw2H0kdipZ_JmG-l1qjiHIb8&ust=1730861770169000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCJCZx9qYxIkDFQAAAAAdAAAAABAI'
+                  fit: BoxFit.fitWidth,
                   )
               ],
             ))));
